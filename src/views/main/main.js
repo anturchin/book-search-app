0854cb047bb62a42a2162e648db55d41b92f6bd7
ann <tur4intwo@gmail.com>
2023-11-01 14:00:37 +0300

    add state and reactivity

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,17 +1,36 @@
 import { AbstractView } from '../../common/view';
+import onChange from 'on-change';
 
 export class MainView extends AbstractView {
 
-	constructor() {
+	state = {
+		list: [],
+		loading: false,
+		serachQuery: undefined,
+		offset: 0,
+	}
+
+	constructor(appState) {
 		super();
+		this.appState = appState;
+		this.appState = onChange(this.appState, this.appStateHook.bind(this));
 		this.setTitle('search book');
+	}
+
+	appStateHook(path) {
+		console.log(path);
+		if (path === 'favorits') {
+			console.log(path);
+			// this.render();
+		}
 	}
 
 	render() {
 		const main = document.createElement('div');
-		main.innerHTML = 'test';
+		main.innerHTML = `number of books: ${this.appState.favorits.length}`;
 		this.app.innerHTML = '';
 		this.app.append(main);
+		this.appState.favorits.push('1');
 	}
 
 }
